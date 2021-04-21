c02a6cde226ecb993edf3f526e7c61e442349de1
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-04-20 20:47:13 -0500

    Building the navbar

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBUxE5A2DkZo4C0NcuhSEEFQvHt6htd3C8",
+    authDomain: "netflix-build-9880f.firebaseapp.com",
+    projectId: "netflix-build-9880f",
+    storageBucket: "netflix-build-9880f.appspot.com",
+    messagingSenderId: "634022751561",
+    appId: "1:634022751561:web:31cd85c5802c94fd967e98",
+    measurementId: "G-RDCFS8T8XD"
+};
