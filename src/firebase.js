d91f47b5d4ec25396b190b01d3474f57582766a1
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2022-10-07 14:52:06 -0500

    Initial setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAmlprvG_rY3sppGThxl19IyaWZnLyvz-w",
+    authDomain: "netflix-7aad1.firebaseapp.com",
+    projectId: "netflix-7aad1",
+    storageBucket: "netflix-7aad1.appspot.com",
+    messagingSenderId: "964033775766",
+    appId: "1:964033775766:web:3ba319ab77a27dd3b21ab7",
+    measurementId: "G-XSVVHYCSGQ"
+};
